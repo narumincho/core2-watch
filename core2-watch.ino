ae1497868c4f039002a26a812d9658373af84273
narumincho <narumincho.starfy@gmail.com>
2021-06-21 18:14:51 +0900

    touch test

--- core2-watch.ino
+++ core2-watch.ino
@@ -2,11 +2,59 @@
 
 void setup()
 {
-    M5.begin();
+  M5.begin(true, true, true, true);
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.setTextColor(BLACK, WHITE);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(10, 10);
+  M5.Lcd.printf("DISPPLAY Test!");
 }
 
 void loop()
 {
-    M5.Lcd.drawLine(0, 0, random(0, 320), random(0, 240), random(0, 65535));
-    delay(26);
+  static bool touch = false;
+  static uint8_t colorOld = -1;
+  TouchPoint_t pos = M5.Touch.getPressPoint();
+  if (pos.x != -1)
+  {
+    touch = true;
+    M5.Axp.SetLed(0);
+  }
+  if (pos.y > 240)
+  {
+    uint8_t color;
+    if (pos.x < 109)
+    {
+      M5.Lcd.setTextColor(RED, WHITE);
+      color = 1;
+    }
+    else if (pos.x > 218)
+    {
+      M5.Lcd.setTextColor(BLUE, WHITE);
+      color = 2;
+    }
+    else if (pos.x >= 109 && pos.x <= 218)
+    {
+      M5.Lcd.setTextColor(GREEN, WHITE);
+      color = 3;
+    }
+
+    if (color != colorOld)
+    {
+      colorOld = color;
+      M5.Axp.SetLDOEnable(3, true);
+      delay(100);
+      M5.Axp.SetLDOEnable(3, false);
+    }
+
+    M5.Lcd.setCursor(0, 26);
+    M5.Lcd.printf(" X:%3d\n Y:%3d", pos.x, pos.y);
+  }
+  else
+  {
+    touch = false;
+    colorOld = -1;
+    M5.Axp.SetLed(1);
+  }
+  delay(10);
 }
