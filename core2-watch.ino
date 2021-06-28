fabece786e3e09e4a91ff601c8a7a44965a0878b
narumincho <narumincho.starfy@gmail.com>
2021-06-28 13:12:29 +0900

    取得した時刻を表示するように

--- core2-watch.ino
+++ core2-watch.ino
@@ -1,21 +1,34 @@
 #include <M5Core2.h>
 
+RTC_TimeTypeDef nowTime;
+RTC_DateTypeDef nowDate;
+
+char timeStrbuff[64];
+
 void setup()
 {
   M5.begin();
-  M5.Lcd.setCursor(20, 20);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.print("2021-0");
-  M5.Lcd.setTextSize(3);
-  M5.Lcd.print("6-28");
-  M5.Lcd.setCursor(20, 60);
-  M5.Lcd.setTextSize(4);
-  M5.Lcd.print("12:34");
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.print(":56'78");
-  M5.Lcd.println(M5.RTC.Second);
 }
 
 void loop()
 {
+  M5.Rtc.GetDate(&nowDate);
+  M5.Rtc.GetTime(&nowTime);
+
+  DisplayDateTime(nowDate.Year, nowDate.Month, nowDate.Date, nowTime.Hours, nowTime.Minutes, nowTime.Seconds);
+  delay(16);
+}
+
+void DisplayDateTime(uint8_t year, uint8_t month, uint8_t day, uint8_t hours, uint8_t minutes, uint8_t seconds)
+{
+  M5.Lcd.setCursor(20, 20);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.print(String(year));
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.print("-" + String(month) + "-" + String(day));
+  M5.Lcd.setCursor(20, 60);
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.print(String(hours) + ":" + String(minutes));
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.print(":" + String(seconds));
 }
