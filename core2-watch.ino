e7cbfa67d5f7f3d0e61d9493a586578d64a9b25b
narumincho <narumincho.starfy@gmail.com>
2021-06-28 12:40:03 +0900

    時刻っぽいものを表示

--- core2-watch.ino
+++ core2-watch.ino
@@ -2,11 +2,20 @@
 
 void setup()
 {
-    M5.begin();
+  M5.begin();
+  M5.Lcd.setCursor(20, 20);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.print("2021-0");
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.print("6-28");
+  M5.Lcd.setCursor(20, 60);
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.print("12:34");
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.print(":56'78");
+  M5.Lcd.println(M5.RTC.Second);
 }
 
 void loop()
 {
-    M5.Lcd.drawLine(0, 0, random(0, 320), random(0, 240), random(0, 65535));
-    delay(26);
 }
